bdb8d7270b94fdeee982c676c08e99aa99e68497
Efim Pushkarev <pushkarevev@yandex.ru>
2020-08-26 20:40:53 +0300

    Removed Footer from HomePage and made <hr/> thickness == 1px

--- src/client/components/homepage/HomePage.js
+++ src/client/components/homepage/HomePage.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Content_InfoBox } from './Content_InfoBox';
 import { Content_Clients } from './Content_Clients';
-import { Footer } from '../Footer';
 
 export const HomePage = () => {
     return (
         <div className={'homepage'}>
             <Content_InfoBox />
             <Content_Clients />
-            <Footer />
         </div>
     );
 };

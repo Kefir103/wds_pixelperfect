98e5622588d383c833065631f80af2c6b3a2bb60
Efim Pushkarev <pushkarevev@yandex.ru>
2020-08-27 21:23:22 +0300

    Added express-history-api-fallback to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fallback = require('express-history-api-fallback');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static('build'));
 app.use(express.static('public'));
+app.use(fallback('index.html', { root: path.join(__dirname, 'build') }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
